64e9593c26eac115181bbcec3752a33b6ed9a97f
Florent Chevallier <florent.chevallier@gmail.com>
2018-11-08 22:03:40 +0100

    affectation

--- rendu_intermediaire/ens/Etage.py
+++ rendu_intermediaire/ens/Etage.py
@@ -1,6 +1,6 @@
 class Etage:
-	def __init__(Etablissement, self, numero_etage, surface, affectation_principale):
+	def __init__(Etablissement, self, numero_etage, surface, affectation):
 		"""Etablissement.__init__(self)"""
 		self.numero_etage = 0
 		self.surface = 0.0
-		self.affectation_principale = None	
+		self.affectation = None	
